6adf141810e9d322188c4109fcf23cbbc01ab60c
キノ0510 <kinont0510@gmail.com>
2024-11-09 15:59:58 +0900

    🚧add APIkey

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2XBk0hB_Ye5yIZdWG65WqpBafRVGEx1s",
-    authDomain: "householdbookapp.firebaseapp.com",
-    projectId: "householdbookapp",
-    storageBucket: "householdbookapp.firebasestorage.app",
-    messagingSenderId: "635723807440",
-    appId: "1:635723807440:web:96f6f38d45d80313fafcd5"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app);
