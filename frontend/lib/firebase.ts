6f2d780c071f3088062d79696d9690a66f0ceb8b
キノ0510 <kinont0510@gmail.com>
2024-11-09 16:03:22 +0900

    🚑️security adjust

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,12 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2XBk0hB_Ye5yIZdWG65WqpBafRVGEx1s",
-    authDomain: "householdbookapp.firebaseapp.com",
-    projectId: "householdbookapp",
-    storageBucket: "householdbookapp.firebasestorage.app",
-    messagingSenderId: "635723807440",
-    appId: "1:635723807440:web:96f6f38d45d80313fafcd5"
+
 };
 
 const app = initializeApp(firebaseConfig)
